308df1e4d5e542c18485c142f3caa295827c53a6
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-09 23:53:59 -0300

    Alterando tabela

--- consulta.sql
+++ consulta.sql
@@ -7,7 +7,7 @@ create table times (
 	ativo boolean
 )
 
-insert into times (nome, pontos, ativo) values ('Time12', 10, 1);
+insert into times (nome, pontos, ativo) values ('time 13', 10, 1);
 
 select * from times;
 select nome from times;
@@ -19,3 +19,7 @@ select * from times where pontos > 3 AND ativo = 1;
 update times set pontos = 11 where id = 1;
 update times set pontos = 11 where id in (1, 2, 3);
 update times set pontos = 10 where not id = 1;
+
+alter table times modify column nome text not null;
+alter table times modify column pontos double DEFAULT '0';
+alter table times add column foto text;
