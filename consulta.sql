c6a2af39f3c46ff4242e9f9252cb07fe7caaed9a
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-10 00:22:23 -0300

    join

--- consulta.sql
+++ consulta.sql
@@ -23,3 +23,24 @@ update times set pontos = 10 where not id = 1;
 alter table times modify column nome text not null;
 alter table times modify column pontos double DEFAULT '0';
 alter table times add column foto text;
+
+select sum(pontos) from times;
+
+create table jogadores (
+	id int not null auto_increment,
+	nome VARCHAR(100) not null,
+	email varchar(255) not null,
+	PRIMARY key(id)
+)
+
+select * from jogadores;
+
+insert into jogadores (nome, email) values ('nome6', 'email6');
+
+alter table jogadores add column time_id int not null;
+
+update jogadores set time_id = 2 where id = 6;
+
+select * from times, jogadores;
+select * from jogadores join times on jogadores.time_id = time_id;
+alter table jogadores add FOREIGN key (time_id) REFERENCES times(id);
